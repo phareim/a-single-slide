3c60108308c1aa711262232cae059ea92fe6345c
Petter Hareim <petter.hareim@hkraft.no>
2021-03-21 20:30:30 +0100

    add a so far useless menu. Text now displays centered, and gets focus as expected.

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -1,5 +1,7 @@
 <template>
-<div contenteditable class="editor" autofocus> </div>
+  <div class="editor" @click="focus">
+    <div class="text" id="text" contenteditable autofocus></div>
+  </div>
 </template>
 
 <script setup>
@@ -8,22 +10,24 @@ import { defineProps, reactive } from 'vue'
 defineProps({
   msg: String
 })
-
-const state = reactive({charCount:0, lineCount: 0})
+const focus = () => {
+  document.getElementById("text").focus();
+}
+const state = reactive({charCount:0, lineCount: 0 })
 </script>
 
 <style scoped>
 @import url('https://fonts.googleapis.com/css2?family=Raleway:wght@444&display=swap');
 .editor {
-      font-family: 'Raleway', sans-serif;
-      color: #eeeeee;
-      font-size: min(4rem, 6vw);     
-      padding: 10vh 5vw;
-      width: 88vw;
-      height: 78vh;
-      text-align: center;
-      display: flex;
-      justify-content:center;
-      flex-direction: column;
-    }
+  font-family: 'Raleway', sans-serif;
+  color: #eeeeee;
+  font-size: min(4rem, 6vw);     
+  padding: 10vh 5vw;
+  width: 88vw;
+  height: 78vh;
+  display: flex;
+  justify-content:center;
+  flex-direction: column;
+  text-align: center;
+}
 </style>
---
+++ src/components/Menu.vue
@@ -0,0 +1,15 @@
+<template>
+  
+</template>
+
+<script setup>
+import { defineProps, reactive } from 'vue'
+
+defineProps({})
+
+const state = reactive({})
+</script>
+
+<style>
+
+</style>
