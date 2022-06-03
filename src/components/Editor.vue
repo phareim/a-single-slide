b9c22ac2853aaa0bae95ede0d5c4ca3fba7ef6da
Petter Hareim <petter.hareim@hkraft.no>
2022-06-03 10:44:38 +0200

    🧹🧹🧹

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -5,28 +5,28 @@
 </template>
 
 <script setup>
-import { defineProps, reactive } from 'vue'
+import { defineProps, reactive } from "vue";
 
 defineProps({
-  msg: String
-})
+  msg: String,
+});
 const focus = () => {
   document.getElementById("text").focus();
-}
-const state = reactive({charCount:0, lineCount: 0 })
+};
+const state = reactive({ charCount: 0, lineCount: 0 });
 </script>
 
 <style scoped>
-@import url('https://fonts.googleapis.com/css2?family=Raleway:wght@444&display=swap');
+@import url("https://fonts.googleapis.com/css2?family=Raleway:wght@444&display=swap");
 .editor {
-  font-family: 'Raleway', sans-serif;
+  font-family: "Raleway", sans-serif;
   color: #eeeeee;
-  font-size: min(4rem, 6vw);     
+  font-size: min(4rem, 6vw);
   padding: 10vh 5vw;
   width: 88vw;
   height: 78vh;
   display: flex;
-  justify-content:center;
+  justify-content: center;
   flex-direction: column;
   text-align: center;
 }
