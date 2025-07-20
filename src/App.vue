97c9fae2d7df0730d68a6f13aed63fa09c9f243b
Petter Hareim <petter.hareim@miles.no>
2025-07-20 11:15:40 +0200

    now with option to set a background-color.

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,23 @@
 <template>
-  <div class="frame">
+  <div class="frame" :style="{ backgroundColor: bgColor }">
     <editor />
   </div>
 </template>
 
 <script setup>
+import { ref, onMounted } from "vue";
 import Editor from "./components/Editor.vue";
 import Menu from "./components/Menu.vue";
+
+const bgColor = ref("rgb(41, 37, 36)");
+
+onMounted(() => {
+  const params = new URLSearchParams(window.location.search);
+  const bgParam = params.get("bg");
+  if (bgParam && /^#?[0-9A-Fa-f]{6}$/.test(bgParam)) {
+    bgColor.value = bgParam.startsWith("#") ? bgParam : `#${bgParam}`;
+  }
+});
 </script>
 
 <style>
